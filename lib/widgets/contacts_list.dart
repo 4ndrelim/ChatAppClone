11825315fd3046e39ac9fc0be63dc4d152a1c85c
Claudeon Susanto <claudeon9j6@gmail.com>
2022-06-27 14:14:32 +0800

    Resolve overflow issue on mobile

--- lib/widgets/contacts_list.dart
+++ lib/widgets/contacts_list.dart
@@ -9,64 +9,60 @@ class ContactsList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 10),
-      child: Column(
-        children: [
-          ListView.builder(
-            shrinkWrap: true,
-            itemCount: info.length,
-            itemBuilder: (context, index) {
-              return Column(
-                children: [
-                  InkWell(
-                    // ! make shit appears to be clickable (touch ripples)
-                    onTap: () {},
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          top: 8.0, bottom: 8.0, right: 8.0),
-                      child: ListTile(
-                        // tiles containing user avatars and messages
-                        leading: CircleAvatar(
-                          backgroundImage: NetworkImage(
-                            info[index]['profilePic'].toString(),
-                          ),
-                          radius: 30,
-                        ),
-                        title: Text(
-                          // * users' names
-                          info[index]['name'].toString(),
-                          style: const TextStyle(
-                            fontSize: 18,
-                          ),
-                        ),
-                        subtitle: Padding(
-                          // * last message sent/received
-                          padding: const EdgeInsets.only(top: 6),
-                          child: Text(
-                            info[index]['message'].toString(),
-                            style: const TextStyle(
-                              fontSize: 15,
-                            ),
-                          ),
-                        ),
-                        trailing: Text(
-                          info[index]['time'].toString(),
-                          style: const TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey,
-                          ),
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: info.length,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              InkWell(
+                // ! make shit appears to be clickable (touch ripples)
+                onTap: () {},
+                child: Padding(
+                  padding:
+                      const EdgeInsets.only(top: 8.0, bottom: 8.0, right: 8.0),
+                  child: ListTile(
+                    // tiles containing user avatars and messages
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        info[index]['profilePic'].toString(),
+                      ),
+                      radius: 30,
+                    ),
+                    title: Text(
+                      // * users' names
+                      info[index]['name'].toString(),
+                      style: const TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    subtitle: Padding(
+                      // * last message sent/received
+                      padding: const EdgeInsets.only(top: 6),
+                      child: Text(
+                        info[index]['message'].toString(),
+                        style: const TextStyle(
+                          fontSize: 15,
                         ),
                       ),
                     ),
+                    trailing: Text(
+                      info[index]['time'].toString(),
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey,
+                      ),
+                    ),
                   ),
-                  const Divider(
-                    color: dividerColor,
-                    indent: 1,
-                  ),
-                ],
-              );
-            },
-          ),
-        ],
+                ),
+              ),
+              const Divider(
+                color: dividerColor,
+                indent: 1,
+              ),
+            ],
+          );
+        },
       ),
     );
   }
