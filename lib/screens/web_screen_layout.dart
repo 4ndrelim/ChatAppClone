67a03858c34190ca15ba408bc36a0e1af5e93736
Claudeon Susanto <claudeon9j6@gmail.com>
2022-06-28 18:44:58 +0800

    test

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/widgets/contacts_list.dart';
 import 'package:whatsapp_clone/widgets/web_profile_bar.dart';
+import 'package:whatsapp_clone/widgets/web_search_bar.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({Key? key}) : super(key: key);
@@ -20,6 +21,7 @@ class WebScreenLayout extends StatelessWidget {
                 // TODO: web profile on top
                 WebProfileBar(),
                 // TODO: search bar
+                WebSearchBar(),
                 ContactsList(),
               ],
             ),
