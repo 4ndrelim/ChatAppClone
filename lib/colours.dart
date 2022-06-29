f7b6445ce28dd1a589cb81bad5ae555948c725f9
Claudeon Susanto <claudeon9j6@gmail.com>
2022-06-29 17:56:58 +0800

    Add web chat input widget for web_screen_layout

--- lib/colours.dart
+++ lib/colours.dart
@@ -5,6 +5,7 @@ const tabColor = Color.fromARGB(255, 255, 255, 255);
 const appBarColor = Color.fromARGB(255, 0, 22, 53);
 const dividerColor = Color.fromARGB(255, 92, 92, 92);
 const webAppBarColor = Color.fromRGBO(42, 47, 50, 1);
+const chatBarMessage = Color.fromRGBO(30, 36, 40, 1);
 
 // own colours
 const appBarTextColor = Color.fromARGB(255, 138, 255, 239);
--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -3,6 +3,7 @@ import 'package:whatsapp_clone/widgets/chat_list.dart';
 import 'package:whatsapp_clone/widgets/contacts_list.dart';
 import 'package:whatsapp_clone/widgets/web_chat_appbar.dart';
 import 'package:whatsapp_clone/widgets/web_profile_bar.dart';
+import 'package:whatsapp_clone/colours.dart';
 import '../widgets/web_search_bar.dart';
 
 class WebScreenLayout extends StatelessWidget {
@@ -47,6 +48,63 @@ class WebScreenLayout extends StatelessWidget {
                   child: ChatList(),
                 ),
                 // Message Input Box
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.07,
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: dividerColor,
+                      ),
+                    ),
+                    color: chatBarMessage,
+                  ),
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.emoji_emotions_outlined),
+                        color: Colors.grey,
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.attach_file),
+                        color: Colors.grey,
+                      ),
+                      // ! Important to add Expanded for TextField !
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            left: 10,
+                            right: 15,
+                            top: 6,
+                            bottom: 6,
+                          ),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: searchBarColor,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: const BorderSide(
+                                  width: 0,
+                                  style: BorderStyle.none,
+                                ),
+                              ),
+                              hintText: 'Type a message ...',
+                              contentPadding: const EdgeInsets.only(left: 20),
+                            ),
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.mic),
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
