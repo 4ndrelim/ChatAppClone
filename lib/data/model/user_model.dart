61cb695222c90e5117f17cd4cc017a57f4113025
Claudeon Susanto <claudeon9j6@gmail.com>
2022-07-20 11:15:56 +0800

    Add UserModel
    https://www.youtube.com/watch?v=tPvW5dsPLvE&list=PLbgNPmiSEwEaNdJ7OpU4giml-71YvhHIE&index=3 at timestamp 18:20, DocumentSnapshot class is undefined

---
+++ lib/data/model/user_model.dart
@@ -0,0 +1,45 @@
+import 'package:whatsapp_clone/domain/entities/user_entity.dart';
+
+class UserModel extends UserEntity {
+  UserModel({
+    required String name,
+    required String email,
+    required String phoneNumber,
+    required bool isOnline,
+    required String uid,
+    required String status,
+    required String profileUrl,
+  }) : super(
+          name: name,
+          email: email,
+          phoneNumber: phoneNumber,
+          isOnline: isOnline,
+          uid: uid,
+          status: status,
+          profileUrl: profileUrl,
+        );
+
+  factory UserModel.fromSnapshot(DocumentSnapshot snapshot) {
+    return UserModel(
+      name: snapshot.data()['name'],
+      email: snapshot.data()['email'],
+      phoneNumber: snapshot.data()['phoneNumber'],
+      uid: snapshot.data()['uid'],
+      isOnline: snapshot.data()['isOnline'],
+      profileUrl: snapshot.data()['profileUrl'],
+      status: snapshot.data()['status'],
+    );
+  }
+
+  Map<String, dynamic> toDocument() {
+    return {
+      "name": name,
+      "email": email,
+      "phoneNumber": phoneNumber,
+      "uid": uid,
+      "isOnline": isOnline,
+      "profileUrl": profileUrl,
+      "status": status,
+    };
+  }
+}
